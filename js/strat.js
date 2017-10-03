e0079348a790cffa4ecc8ba9ebebe2c6ff782217
Kyriaki <clemgam2@gmail.com>
2017-10-03 23:12:31 +0200

    Copy Set button enabled

--- js/strat.js
+++ js/strat.js
@@ -1,7 +1,14 @@
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
+function copyToClipboard(id) {
+	var stratSet = document.getElementById(id).innerHTML;
+	window.alert(stratSet);
+	//window.clipboardData.setData("text/plain", strat);
+	//var dummy = document.createElement("textarea");
+	//dummy.innerHTML = strat;
+	//dummy.select();
+	//document.execCommand("copy");
+	//document.body.removeChild(dummy);
+}
+
+function test(){
+	window.alert("OK!");
 }
